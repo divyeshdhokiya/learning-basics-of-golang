50c6e4efd822c5b5f21e3a3a49f80b36db5803eb
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 11:30:45 +0530

    Variables: type conversions

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -1,41 +1,25 @@
 package main
 
-import "fmt"
-
-/* Package level variables. can not use ":=" syntax at this place */
-var (
-	p   int    = 30
-	str string = "Hi!"
+import (
+	"fmt"
+	"strconv"
 )
 
 func main() {
-	var num int
-	/* Print default value */
-	fmt.Println(num) // OP: 0
+	/* Convert int to float32 */
+	var i int = 20
+	fmt.Printf("%v, %T", i, i) // OP: 20, int
 
-	/* Ways of declaring variables */
-	/* 1: Need to declare but not ready to initialize it right away */
-	var i int
-	i = 20
-	// Examples
-	fmt.Println(i) // OP: 20
+	var j float32 = 21
+	/* Convert to float32 */
+	j = float32(i)
+	fmt.Printf("%v, %T", j, j) // OP: 20, float32
 
-	/* 2: When go does not have enough information of the type of the value assigned to the variable */
-	var j int = 22
-	var l float32 = 1.5
-	// Examples
-	fmt.Printf("%v, %T", j, j) // OP: 22, int
-	fmt.Printf("%v, %T", l, l) // OP: 1.5 float32
+	/* Convert int to string */
+	var l int = 22
+	fmt.Printf("%v, %T", l, l) // OP: 20, int
 
-	/* 3: short and clean way */
-	k := 21
-	fmt.Printf("%v, %T", k, k)
-
-	fmt.Printf("%v, %T", p, p)
-	fmt.Printf("%v, %T", str, str)
-
-	/* Variable shadowing */
-	p = 100
-	fmt.Printf("%v, %T", p, p)
-
+	var k string = "str"
+	k = strconv.Itoa(l)
+	fmt.Printf("%v, %T", k, k) // OP: 22, string
 }

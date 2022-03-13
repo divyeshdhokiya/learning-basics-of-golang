099818eb59f11ad629ff6d3edb8c2ecc24bdb938
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 14:26:46 +0530

    Primitives: Rune

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -3,19 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	/* Text */
-	s := "This is string"
-	fmt.Printf("%v,%T\n", s, s)                       // OP: This is string,string
-	fmt.Printf("%v,%T\n", string(s[1]), string(s[1])) // OP: h,string
-
-	// Concat
-	s1 := "str1"
-	s2 := "str2"
-	fmt.Printf("%v,%T", s1+s2, s1+s2) // OP: str1str2,string
-
-	// Convert to collection of bytes
-	bs1 := "str1"
-	bs2 := []byte(bs1)
-	fmt.Printf("%v,%T", bs2, bs2) // OP: [115 116 114 49],[]uint8
+	/* Rune */
+	s := 'a'
+	fmt.Printf("%v,%T\n", s, s) // OP: 97, int32
 
 }

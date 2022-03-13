85a9b052050e7ea83677ebea0104bc8ac8e3c5da
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 14:03:55 +0530

    Primitives: numeric - Int

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -1,17 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	/* Boolean */
-	var n bool = false
-	fmt.Printf("%v, %T", n, n)
+	/* Numeric */
+	// Signed Interger
+	n := 32
+	fmt.Printf("%v,%T", n, n)
 
-	a := 1 == 1
-	fmt.Printf("%v,%T", a, a)
-	b := 12 == 13
-	fmt.Printf("%v,%T", b, b)
+	// Unsigned Integer
+	var un uint16 = 42
+	fmt.Printf("%v, %T", un, un)
 
+	// bit operators
+	a := 10             // 1010
+	b := 3              // 0011
+	fmt.Println(a & b)  // 0010 -> 2
+	fmt.Println(a | b)  // 1011 -> 11
+	fmt.Println(a ^ b)  // 1001 -> 9
+	fmt.Println(a &^ b) // 0100 ->  8
+
+	// bit shifting
+	c := 8              // 2^3
+	fmt.Println(c << 3) // 2^3 * 2^3 = 2^6 -> 64
+	fmt.Println(c >> 3) // 2^3 / 2^3 = 2^0 -> 1
+
+	
 }

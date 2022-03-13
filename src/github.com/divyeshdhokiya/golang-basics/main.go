089bba25f8a7ab4f51ce1c47aee34703bf631d71
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 19:26:39 +0530

    Arrays

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -2,38 +2,49 @@ package main
 
 import "fmt"
 
-const (
-	_ = iota + 5
-	a
-	b
-	c
-)
-
-const (
-	_  = iota
-	KB = 1 << (10 * iota)
-	MB
-	GB
-)
-
-const (
-	isAdmin = 1 << iota
-	isUser
-)
-
 func main() {
-	/* Numerated const */
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
+	/* Arrays */
+	// 1. predefined size
+	students := [3]int{1, 2, 3}
+	fmt.Printf("%v\n", students) // OP: [1 2 3]
 
-	// Example 1
-	fileSize := 4000000000.
-	fmt.Printf("%.2fGB\n", fileSize/GB)
-	fmt.Printf("%.2fMB\n", fileSize/MB)
+	// 2. dynamic size
+	dStudents := [...]int{1, 2, 3, 4}
+	fmt.Printf("%v\n", dStudents) // OP: [1 2 3 4]
 
-	// Example 2
-	var roles byte = isAdmin
-	fmt.Printf("isAdmin: %v\n", isAdmin&roles == isAdmin) // true
-	fmt.Printf("isUser: %v\n", isUser&roles == isUser)    // false
+	arr := []int{1, 2, 3, 4}
+	fmt.Printf("%v\n", arr) // OP: [1 2 3 4]
+
+	var students1 [3]string
+	fmt.Printf("%v\n", students1) // OP: [ ]
+	students1[0] = "First"
+	students1[1] = "Second"
+	students1[2] = "Third"
+	fmt.Printf("%v\n", students1)      // OP: [First Second Third]
+	fmt.Printf("%v\n", len(students1)) // OP: 3
+
+	// ---
+	matrix := [3][3]int{{1, 2, 3}, {4, 5, 6}}
+
+	fmt.Printf("%v\n", matrix) // OP: [[1 2 3] [4 5 6] [0 0 0]]
+
+	matrix[2] = [...]int{6, 7, 8}
+	fmt.Printf("%v\n", matrix) // OP: [[1 2 3] [4 5 6] [6 7 8]]
+
+	arr1 := [...]int{1, 2, 3}
+
+	// Copy array: value
+	arr2 := arr1
+	arr2[2] = 4
+
+	fmt.Printf("%v\n", arr1) // OP: [1 2 3]
+	fmt.Printf("%v\n", arr2) // OP: [1 2 4]
+
+	// Copy array: reference
+	arr3 := &arr1
+	arr3[1] = 9
+
+	fmt.Printf("%v\n", arr1) // OP: [1 9 3]
+	fmt.Printf("%v\n", arr3) // OP: &[1 9 3]
+
 }

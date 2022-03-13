70d9cf814b8e5e7b2e59c5273401ea68ee60fbda
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-14 00:19:10 +0530

    Tags

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -1,60 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type Animal struct {
-	Name   string
+	Name   string `required! max:"100"`
 	Origin string
 }
 
-type Bird struct {
-	Animal
-	Speed  float32
-	CanFly bool
-}
-
-type Car struct {
-	Model string
-	Year  int
-	Color string
-	Parts []string
-}
-
 func main() {
-	/* Struct */
-	aCar := Car{
-		Model: "Toyota",
-		Year:  2000,
-		Color: "White",
-		Parts: []string{
-			"Doors",
-			"Wheels",
-		},
-	}
-
-	fmt.Println(aCar)
-
-	// Fetch
-	fmt.Println(aCar.Model)    // OP: Toyota
-	fmt.Println(aCar.Parts)    // OP: [Doors Wheels]
-	fmt.Println(aCar.Parts[1]) // OP: Wheels
-
-	/* Anonymous struct */
-	aDr := struct{ name string }{name: "Dr X"}
-	fmt.Println(aDr) // OP: {Dr X}
-	bDr := aDr
-	bDr.name = "Dr Z"
-	fmt.Println(bDr) // OP: {Dr Z}
-
-	// Note: use & to manipulate main struct
-
-	/* Embedded */
-	b := Bird{}
-	b.Name = "Sparrow"
-	b.Origin = "IN"
-	b.CanFly = true
-	b.Speed = 10
-
-	fmt.Println(b) // OP: {{Sparrow IN} 10 true}
-
+	/* Tags */
+	t := reflect.TypeOf((Animal{}))
+	field, _ := t.FieldByName(("Name"))
+	fmt.Println(field.Tag)
 }

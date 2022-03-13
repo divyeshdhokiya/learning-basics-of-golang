3ef32518a93df72b98a24a8c153f8fd11a185bdf
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 14:22:03 +0530

    Primitives: text

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	/* Complex */
-	var n complex64 = 1 + 2i
-	var n128 complex128 = 1 + 2i
-	fmt.Printf("%v,%T\n", n, n)       // OP: (1+2i),complex64
-	fmt.Printf("%v,%T\n", n128, n128) // OP: (1+2i),complex128
+	/* Text */
+	s := "This is string"
+	fmt.Printf("%v,%T\n", s, s)                       // OP: This is string,string
+	fmt.Printf("%v,%T\n", string(s[1]), string(s[1])) // OP: h,string
+
+	// Concat
+	s1 := "str1"
+	s2 := "str2"
+	fmt.Printf("%v,%T", s1+s2, s1+s2) // OP: str1str2,string
+
+	// Convert to collection of bytes
+	bs1 := "str1"
+	bs2 := []byte(bs1)
+	fmt.Printf("%v,%T", bs2, bs2) // OP: [115 116 114 49],[]uint8
 
 }

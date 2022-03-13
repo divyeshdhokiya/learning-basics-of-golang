de9f8bd2cee29737c658a5adb5aed335ed631009
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 13:42:09 +0530

    Primitives: boolean

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 )
 
-var I int = 10 // First letter Uppercase: Export variable
-var i int = 20 // First letter lowercase: Package level variable
-
-// Notes:
-// No private scope, block level scope
-// pascal or camelCase
-
 func main() {
-	fmt.Printf("%v, %T", I, I) // OP: 10, int
-	fmt.Printf("%v, %T", i, i) // OP: 20, int
+	/* Boolean */
+	var n bool = false
+	fmt.Printf("%v, %T", n, n)
+
+	a := 1 == 1
+	fmt.Printf("%v,%T", a, a)
+	b := 12 == 13
+	fmt.Printf("%v,%T", b, b)
 
 }

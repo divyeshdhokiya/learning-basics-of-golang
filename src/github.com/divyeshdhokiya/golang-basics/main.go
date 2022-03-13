86d101c0349f33dbfea7d7a373dfa27b1e569440
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-14 00:59:59 +0530

    swtch case

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -8,10 +8,22 @@ func main() {
 	/* "switch" statement */
 
 	switch 2 {
-	case 1:
+	case 1, 2:
 		fmt.Println("First case")
+	case 3:
+		fmt.Println("Second case")
 	default:
-		fmt.Println("default case")
+		fmt.Println("Second case")
+
+	}
+
+	/* Tagless syntax */
+	i := 10
+	switch {
+	case i <= 10:
+		fmt.Println("Less than")
+	case i <= 20:
+		fmt.Println("Less than")
 
 	}
 

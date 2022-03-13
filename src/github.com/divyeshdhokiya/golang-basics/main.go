c91bc63419f931c8b838d923c9fea77bd6645fd9
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-14 00:48:54 +0530

    if statement

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type Animal struct {
-	Name   string `required! max:"100"`
-	Origin string
-}
-
 func main() {
-	/* Tags */
-	t := reflect.TypeOf((Animal{}))
-	field, _ := t.FieldByName(("Name"))
-	fmt.Println(field.Tag)
+	/* "if" statement */
+
+	if true {
+		fmt.Println("inside true block")
+	}
+
+	colors := map[string]string{
+		"Red":   "#123456",
+		"White": "#fff",
+		"Black": "#000",
+	}
+	fmt.Println(colors["Red"])
+
+	if color, ok := colors["White"]; ok {
+		fmt.Println(color, ok) // OP: #fff true
+	}
+
+	if color, ok := colors["Blue"]; ok {
+		fmt.Println(color, ok) // Not executed
+	}
+
+	a := 20
+	b := 40
+	if a < b {
+		fmt.Println("Low")
+	}
+	if a > b {
+		fmt.Println("High")
+	}
+
 }

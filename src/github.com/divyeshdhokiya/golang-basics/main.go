28289d3ab48c26dcd06666d884650c7dc7c4761e
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 23:30:48 +0530

    Maps

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -3,69 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	/* Slices */
-	students := []int{1, 2, 3}
-	fmt.Printf("%v\n", students)      // OP: [1 2 3]
-	fmt.Printf("%v\n", len(students)) // OP: 3
-	fmt.Printf("%v\n", cap(students)) // OP: 3
+	/* Maps */
+	/* 1 */
+	statePopulations := map[string]int{
+		"a": 10,
+		"b": 10,
+		"c": 10,
+		"d": 10,
+	}
+	fmt.Println(statePopulations) // OP: map[a:10 b:10 c:10 d:10]
 
-	a := []int{11, 22, 33, 44, 55, 66, 77, 88}
-	b := a[:]
-	c := a[:3]
-	d := a[5:]
-	e := a[5:7]
-	fmt.Printf("%v\n", a) // OP: [11 22 33 44 55 66 77 88]
-	fmt.Printf("%v\n", b) // OP: [11 22 33 44 55 66 77 88]
-	fmt.Printf("%v\n", c) // OP: [11 22 33]
-	fmt.Printf("%v\n", d) // OP: [66 77 88]
-	fmt.Printf("%v\n", e) // OP: [66 77]
+	/* 2 */
+	statePops := make(map[string]int)
+	statePops = map[string]int{
+		"a": 10,
+		"b": 10,
+		"c": 10,
+		"d": 10,
+	}
+	fmt.Println(statePops) // OP: map[a:10 b:10 c:10 d:10]
 
-	a[5] = 100            // Update original array hence effect to all other arrays
-	fmt.Printf("%v\n", a) // OP: [11 22 33 44 55 100 77 88]
-	fmt.Printf("%v\n", b) // OP: [11 22 33 44 55 100 77 88]
-	fmt.Printf("%v\n", c) // OP: [11 22 33]
-	fmt.Printf("%v\n", d) // OP: [100 77 88]
-	fmt.Printf("%v\n", e) // OP: [100 77]
+	// Read one
+	fmt.Println(statePops["a"]) // OP: 10
 
-	// Make function
-	aa := make([]int, 3, 100)
-	fmt.Printf("%v\n", aa)      // OP: [0 0 0]
-	fmt.Printf("%v\n", len(aa)) // OP: 3
-	fmt.Printf("%v\n", cap(aa)) // OP: 100
+	// Add new
+	statePops["e"] = 10
+	fmt.Println(statePops) // OP: map[a:10 b:10 c:10 d:10, e:10]
 
-	// ---
-	x := []int{}
-	fmt.Printf("%v\n", x)      // OP: []
-	fmt.Printf("%v\n", len(x)) // OP: 0
-	fmt.Printf("%v\n", cap(x)) // OP: 0
+	//Delete
+	delete(statePops, "e")
+	fmt.Println(statePops) // OP: map[a:10 b:10 c:10 d:10]
 
-	x = append(x, 2)
-	fmt.Printf("%v\n", x)      // OP: [2]
-	fmt.Printf("%v\n", len(x)) // OP: 1
-	fmt.Printf("%v\n", cap(x)) // OP: 1
+	fmt.Println(statePops["e"]) // OP: 0
 
-	// x = append(x, 3, 4, 5, 6)
-	// fmt.Printf("%v\n", x)      // OP: [2 3 4 5 6]
-	// fmt.Printf("%v\n", len(x)) // OP: 5
-	// fmt.Printf("%v\n", cap(x)) // OP: 6
+	// is exist
+	key, ok := statePops["e"]
+	fmt.Println(key, ok) // OP: 0, false
 
-	x = append(x, []int{3, 4, 5, 6}...)
-	fmt.Printf("%v\n", x)      // OP: [2 3 4 5 6]
-	fmt.Printf("%v\n", len(x)) // OP: 5
-	fmt.Printf("%v\n", cap(x)) // OP: 6
-
-	// Stack
-	w := []int{1, 2, 3, 4, 5, 6}
-	fmt.Printf("%v\n", w) // OP: [1 2 3 4 5 6]
-	// Shift: remove first element
-	y := w[1:]
-	fmt.Printf("%v\n", y) // OP: [2 3 4 5 6]
-	// Shift: remove last element
-	y2 := w[:len(w)-1]
-	fmt.Printf("%v\n", y2) // OP: [1 2 3 4 5]
-	// Shift: remove n element -> 3
-	y3 := append(w[:2], w[3:]...)
-	fmt.Printf("%v\n", y3) // OP: [1 2 4 5 6]
-
-	
 }

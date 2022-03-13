3c0aada4419432222eb0f79e00494663b907eded
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 11:37:27 +0530

    Variables: visibility

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
+var I int = 10 // First letter Uppercase: Export variable
+var i int = 20 // First letter lowercase: Package level variable
+
+// Notes:
+// No private scope, block level scope
+// pascal or camelCase
+
 func main() {
-	/* Convert int to float32 */
-	var i int = 20
+	fmt.Printf("%v, %T", I, I) // OP: 10, int
 	fmt.Printf("%v, %T", i, i) // OP: 20, int
 
-	var j float32 = 21
-	/* Convert to float32 */
-	j = float32(i)
-	fmt.Printf("%v, %T", j, j) // OP: 20, float32
-
-	/* Convert int to string */
-	var l int = 22
-	fmt.Printf("%v, %T", l, l) // OP: 20, int
-
-	var k string = "str"
-	k = strconv.Itoa(l)
-	fmt.Printf("%v, %T", k, k) // OP: 22, string
 }

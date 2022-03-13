4f07844a6768d1609de6b9183bc99471e8f3c4ab
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-13 17:20:09 +0530

    Constant: numerated

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 const (
-	a = iota
-	b = iota
-	c = iota
+	_ = iota + 5
+	a
+	b
+	c
+)
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB
+	GB
+)
+
+const (
+	isAdmin = 1 << iota
+	isUser
 )
 
 func main() {
@@ -13,4 +26,14 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+
+	// Example 1
+	fileSize := 4000000000.
+	fmt.Printf("%.2fGB\n", fileSize/GB)
+	fmt.Printf("%.2fMB\n", fileSize/MB)
+
+	// Example 2
+	var roles byte = isAdmin
+	fmt.Printf("isAdmin: %v\n", isAdmin&roles == isAdmin) // true
+	fmt.Printf("isUser: %v\n", isUser&roles == isUser)    // false
 }

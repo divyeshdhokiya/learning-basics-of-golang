83131426b38a7003c48e182fc9ef738da9d2ed4b
Divyesh Dhokiya <dmdhokiya@gmail.com>
2022-03-15 03:07:06 +0530

    Loops: Simple, exist early, loop through collection

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -5,26 +5,93 @@ import (
 )
 
 func main() {
-	/* "switch" statement */
+	/* Looping */
 
-	switch 2 {
-	case 1, 2:
-		fmt.Println("First case")
-	case 3:
-		fmt.Println("Second case")
-	default:
-		fmt.Println("Second case")
-
+	// Simple Loops
+	for i, j := 0, 0; i < 5; i, j = i+1, j+2 {
+		fmt.Println(i, j)
+		/*
+			OP:
+				0 0
+				1 2
+				2 4
+				3 6
+				4 8
+		*/
 	}
 
-	/* Tagless syntax */
-	i := 10
-	switch {
-	case i <= 10:
-		fmt.Println("Less than")
-	case i <= 20:
-		fmt.Println("Less than")
-
+	k := 0 // scope to main function
+	for k < 5 {
+		fmt.Println(k)
+		k++
 	}
+	/*
+		OP:
+		0
+		1
+		2
+		3
+		4
+	*/
+	fmt.Println("--")
 
+	// Exiting Early
+	l := 0
+	for {
+		if l > 5 {
+			break
+		}
+		fmt.Println(l)
+		l++
+	}
+	/*
+		OP:
+		0
+		1
+		2
+		3
+		4
+		5
+	*/
+	println("--")
+
+	// break out from parent look
+Loop:
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			fmt.Println(i * j)
+			if i*j >= 3 {
+				break Loop
+			}
+		}
+	}
+	/*
+		OP:
+		1
+		2
+		3
+	*/
+	println("--")
+
+	// Looping through collections - for range loop
+	s := []int{1, 2, 3}
+	for m, n := range s {
+		fmt.Println(m, n)
+	}
+	/*
+		OP:
+		0 1
+		1 2
+		2 3
+	*/
+	// Ignore index
+	for _, n := range s {
+		fmt.Println(n)
+	}
+	/*
+		OP:
+		1
+		2
+		3
+	*/
 }

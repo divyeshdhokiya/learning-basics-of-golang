7a2e8d47ee2ae44330d523a4ca3104f975fafc84
Divyesh Dhokiya <ddhokia@saltside.se>
2022-03-15 12:14:09 +0530

    Panic

--- src/github.com/divyeshdhokiya/golang-basics/main.go
+++ src/github.com/divyeshdhokiya/golang-basics/main.go
@@ -3,39 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	/* Defer: invoke a function but delay it's execution   */
-	// c := 10
-	// d := 20
-	// defer fmt.Println(c, d)
-	// e := c + d
-	// fmt.Println(c, d, e)
+	/* Panic:  how app can panic*/
+	// a, b := 1, 0
+	// ans := a / b
+	// fmt.Println(ans) // OP: panic: runtime error: integer divide by zero
 
-	/*
-		OP:
-		10 20 30
-		10 20
+	/* 	fmt.Println("start")
+	   	panic("something went wrong!")
+	   	fmt.Println("end")
+
+	   	/*
+	   		OP:
+	   			start
+	   			panic: something went wrong!
 	*/
 
-	// Order: Normal
-	// fmt.Println(1)
-	// defer fmt.Println(2)
-	// fmt.Println(3)
-	/*
-		OP:
-		1
-		3
-		2
-	*/
+	fmt.Println("start")
+	defer fmt.Println("Deferred")
+	panic("something went wrong!")
+	fmt.Println("end")
 
-	// Order: LIFO - Last in first out
-	defer fmt.Println(1)
-	defer fmt.Println(2)
-	defer fmt.Println(3)
 	/*
 		OP:
-		3
-		2
-		1
+			start
+			Deferred
+			panic: something went wrong!
 	*/
 
 }
